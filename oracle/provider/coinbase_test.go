d7a938066a1f9b25327a8a35fb2e3dd4bb395c5d
Starsquid <info@starsquid.io>
2023-01-23 21:56:48 +0100

    Rework Coinbase to use the new request controller
    The websocket api of Coinbase is not reliably reporting pair prices,
    therefore the rest api needs to be used. The rate limit of 10 requests
    per second and the lack of an enpoint providing all prices at once,
    make it necessary to query each configured pair prior to the GetPrices
    call to be able to provide all requested prices in time.
    
    The new version of the Coinbase provider uses the request controller
    and constantly updates the configured prices in batches of 10 requests
    at once with a short time between the batches to not exceed the rate
    limits. The interval of the request lopp is set automatically according
    to the amount of configured currency pairs.

--- oracle/provider/coinbase_test.go
+++ oracle/provider/coinbase_test.go
@@ -2,7 +2,6 @@ package provider
 
 import (
 	"context"
-	"encoding/json"
 	"testing"
 
 	"price-feeder/oracle/types"
@@ -17,74 +16,80 @@ func TestCoinbaseProvider_GetTickerPrices(t *testing.T) {
 		context.TODO(),
 		zerolog.Nop(),
 		Endpoint{},
-		types.CurrencyPair{Base: "BTC", Quote: "USDT"},
+		testAtomUsdtCurrencyPair,
 	)
 	require.NoError(t, err)
 
 	t.Run("valid_request_single_ticker", func(t *testing.T) {
-		lastPrice := "34.69000000"
-		volume := "2396974.02000000"
-
-		tickerMap := map[string]CoinbaseWsTickerMsg{}
-		tickerMap["ATOM-USDT"] = CoinbaseWsTickerMsg{
-			Price:  lastPrice,
-			Volume: volume,
+		tickers := map[string]types.TickerPrice{}
+		tickers["ATOMUSDT"] = types.TickerPrice{
+			Price:  testAtomPriceDec,
+			Volume: testAtomVolumeDec,
 		}
 
-		p.tickers = tickerMap
+		p.tickers = tickers
 
-		prices, err := p.GetTickerPrices(types.CurrencyPair{Base: "ATOM", Quote: "USDT"})
+		prices, err := p.GetTickerPrices(testAtomUsdtCurrencyPair)
 		require.NoError(t, err)
 		require.Len(t, prices, 1)
-		require.Equal(t, sdk.MustNewDecFromStr(lastPrice), prices["ATOMUSDT"].Price)
-		require.Equal(t, sdk.MustNewDecFromStr(volume), prices["ATOMUSDT"].Volume)
+		require.Equal(
+			t,
+			sdk.MustNewDecFromStr(testAtomPriceString),
+			prices["ATOMUSDT"].Price,
+		)
+		require.Equal(
+			t,
+			sdk.MustNewDecFromStr(testAtomVolumeString),
+			prices["ATOMUSDT"].Volume,
+		)
 	})
 
 	t.Run("valid_request_multi_ticker", func(t *testing.T) {
-		lastPriceAtom := "34.69000000"
-		lastPriceUmee := "41.35000000"
-		volume := "2396974.02000000"
-
-		tickerMap := map[string]CoinbaseWsTickerMsg{}
-		tickerMap["ATOM-USDT"] = CoinbaseWsTickerMsg{
-			Price:  lastPriceAtom,
-			Volume: volume,
+		tickers := map[string]types.TickerPrice{}
+		tickers["ATOMUSDT"] = types.TickerPrice{
+			Price:  testAtomPriceDec,
+			Volume: testAtomVolumeDec,
 		}
 
-		tickerMap["UMEE-USDT"] = CoinbaseWsTickerMsg{
-			Price:  lastPriceUmee,
-			Volume: volume,
+		tickers["BTCUSDT"] = types.TickerPrice{
+			Price:  testBtcPriceDec,
+			Volume: testBtcVolumeDec,
 		}
 
-		p.tickers = tickerMap
+		p.tickers = tickers
+
 		prices, err := p.GetTickerPrices(
-			types.CurrencyPair{Base: "ATOM", Quote: "USDT"},
-			types.CurrencyPair{Base: "UMEE", Quote: "USDT"},
+			testAtomUsdtCurrencyPair,
+			testBtcUsdtCurrencyPair,
 		)
+
 		require.NoError(t, err)
 		require.Len(t, prices, 2)
-		require.Equal(t, sdk.MustNewDecFromStr(lastPriceAtom), prices["ATOMUSDT"].Price)
-		require.Equal(t, sdk.MustNewDecFromStr(volume), prices["ATOMUSDT"].Volume)
-		require.Equal(t, sdk.MustNewDecFromStr(lastPriceUmee), prices["UMEEUSDT"].Price)
-		require.Equal(t, sdk.MustNewDecFromStr(volume), prices["UMEEUSDT"].Volume)
+		require.Equal(
+			t,
+			testBtcPriceDec,
+			prices["BTCUSDT"].Price,
+		)
+		require.Equal(
+			t,
+			testBtcVolumeDec,
+			prices["BTCUSDT"].Volume,
+		)
+		require.Equal(
+			t,
+			testAtomPriceDec,
+			prices["ATOMUSDT"].Price,
+		)
+		require.Equal(
+			t,
+			testAtomVolumeDec,
+			prices["ATOMUSDT"].Volume,
+		)
 	})
 
 	t.Run("invalid_request_invalid_ticker", func(t *testing.T) {
 		prices, err := p.GetTickerPrices(types.CurrencyPair{Base: "FOO", Quote: "BAR"})
-		require.EqualError(t, err, "coinbase failed to get ticker price for FOO-BAR")
+		require.EqualError(t, err, "coinbase failed to get ticker price for FOOBAR")
 		require.Nil(t, prices)
 	})
-}
-
-func TestCoinbaseProvider_GetSubscriptionMsgs(t *testing.T) {
-	provider := &CoinbaseProvider{
-		subscribedPairs: map[string]types.CurrencyPair{},
-	}
-	cps := []types.CurrencyPair{
-		{Base: "ATOM", Quote: "USDT"},
-	}
-	subMsgs := provider.GetSubscriptionMsgs(cps...)
-
-	msg, _ := json.Marshal(subMsgs[0])
-	require.Equal(t, "{\"type\":\"subscribe\",\"product_ids\":[\"ATOM-USDT\"],\"channels\":[\"matches\",\"ticker\"]}", string(msg))
 }
