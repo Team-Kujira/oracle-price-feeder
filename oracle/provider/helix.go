03749b68bf40d7cd9f1a7fbbd3284010964e965e
PFC <81114960+PFC-developer@users.noreply.github.com>
2024-08-07 15:13:14 -0500

    fix: helix requires decimal sizes.

--- oracle/provider/helix.go
+++ oracle/provider/helix.go
@@ -3,6 +3,8 @@ package provider
 import (
 	"context"
 	"encoding/json"
+	"io"
+	"net/http"
 	"strings"
 	"time"
 
@@ -27,6 +29,7 @@ type (
 	HelixProvider struct {
 		provider
 		contracts map[string]string
+		token     map[string]HelixToken
 	}
 
 	HelixMarketsResponse struct {
@@ -35,11 +38,21 @@ type (
 
 	HelixMarket struct {
 		Market struct {
-			Ticker string `json:"ticker"`
+			Ticker     string `json:"ticker"`
+			BaseDenom  string `json:"base_denom"`
+			QuoteDenom string `json:"quote_denom"`
 		} `json:"market"`
 		MidPriceAndTob struct {
 			Price string `json:"mid_price"`
 		} `json:"mid_price_and_tob"`
+	}
+
+	HelixToken struct {
+		Address  string `json:"address"`
+		Decimals uint64 `json:"decimals"`
+		Symbol   string `json:"symbol"`
+		Name     string `json:"name"`
+		Denom    string `json:"denom"`
 	}
 )
 
@@ -47,6 +60,7 @@ func NewHelixProvider(
 	ctx context.Context,
 	logger zerolog.Logger,
 	endpoints Endpoint,
+	helixTokenFile string,
 	pairs ...types.CurrencyPair,
 ) (*HelixProvider, error) {
 	provider := &HelixProvider{}
@@ -58,6 +72,26 @@ func NewHelixProvider(
 		nil,
 		nil,
 	)
+
+	res, err := http.Get(helixTokenFile)
+	if err != nil {
+		return nil, err
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var response []HelixToken
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
+	tokens := map[string]HelixToken{}
+	for _, helixToken := range response {
+		tokens[strings.ToUpper(helixToken.Denom)] = helixToken
+	}
+	provider.token = tokens
 
 	provider.contracts = provider.endpoints.ContractAddresses
 
@@ -84,9 +118,41 @@ func (p *HelixProvider) Poll() error {
 			continue
 		}
 
+		pair, found := p.getPair(market.Market.Ticker)
+		if !found {
+			continue
+		}
+
+		rawPrice := strToDec(market.MidPriceAndTob.Price)
+		if rawPrice.IsNil() {
+			p.logger.Info().Str("ticker", market.Market.Ticker).Msg("No Price available from Helix")
+			continue
+		}
+		baseToken, found := p.token[strings.ToUpper(market.Market.BaseDenom)]
+		if !found {
+			p.logger.Warn().Str("token", pair.Base).Str("denom", market.Market.BaseDenom).Msg("token not found in helix Token list")
+			continue
+		}
+		quoteToken, found := p.token[strings.ToUpper(market.Market.QuoteDenom)]
+		if !found {
+			p.logger.Warn().Str("token", pair.Quote).Str("denom", market.Market.QuoteDenom).Msg("token not found in helix Token list")
+			continue
+		}
+		var decimalDifference uint64
+		if quoteToken.Decimals > baseToken.Decimals {
+			decimalDifference = quoteToken.Decimals - baseToken.Decimals
+		} else {
+			decimalDifference = baseToken.Decimals - quoteToken.Decimals
+		}
+		multiplier := sdk.NewDec(10).Power(decimalDifference)
+		p.logger.Warn().Uint64("decimalDifference", decimalDifference).Str("ticker", market.Market.Ticker).
+			Msg("decimalDifference")
+
+		rawPrice = rawPrice.Mul(multiplier)
+
 		p.setTickerPrice(
 			market.Market.Ticker,
-			strToDec(market.MidPriceAndTob.Price),
+			rawPrice,
 			sdk.OneDec(), // helix doesn't appear to report volume
 			timestamp,
 		)
