dfd35f3289fbddee6341331231e27d02d1dbdb03
Starsquid <22675214-starsquid@users.noreply.gitlab.com>
2025-10-20 13:57:19 +0200

    Increase twap history and allowed gap size

--- oracle/derivative/twap.go
+++ oracle/derivative/twap.go
@@ -13,7 +13,7 @@ import (
 )
 
 const (
-	twapMaxTimeDeltaSeconds      = int64(120)
+	twapMaxTimeDeltaSeconds      = int64(200)
 	twapMinHistoryPeriodFraction = 0.8
 	twapMaxPriceDeviation        = 0.1
 )
@@ -52,7 +52,7 @@ func (d *TwapDerivative) GetPrices(symbol string) (map[string]types.TickerPrice,
 		return nil, fmt.Errorf("pair not configured")
 	}
 
-	start := now.Add(-period)
+	start := now.Add(-period * 2)
 	tickers, err := d.history.GetTickerPrices(symbol, start, now)
 	if err != nil {
 		d.logger.Error().
